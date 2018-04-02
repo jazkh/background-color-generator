e802c6201f06d2d3e7e22c8a0c13d4950477fa9b
jazkh <mjahanzebkhan@yahoo.com>
2018-04-02 10:13:38 +0500

    Converted to ES6 style. Made it more simple

--- script.js
+++ script.js
@@ -1,48 +1,31 @@
-var cc1 = document.getElementById("change1");
-var cc2 = document.getElementById("change2");
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
-var rcolor = document.getElementById("random");
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
+const rcolor = document.getElementById("random");
 
 
-function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+const setGradient = () => {
+	body.style.background = `linear-gradient(to right, 
+	${color1.value}, ${color2.value})`;
 
-	css.textContent = body.style.background + ";";
-}
+	css.textContent = `${body.style.background}`;
+};
 
-function generateColor() {
-    var pos1 = Math.floor((Math.random() * 256));
-    var pos2 = Math.floor((Math.random() * 256));
-    var pos3 = Math.floor((Math.random() * 256));
-
-	var rgbColor = pos1 + "," + pos2 + "," + pos3; 
+const generateColor = () => {
+	const pos1 = Math.floor((Math.random() * 256));
+	const pos2 = Math.floor((Math.random() * 256));
+	const pos3 = Math.floor((Math.random() * 256));
+	const rgbColor = `${pos1, pos2, pos3}`; 
 
 // ---Converting RGB into HEX to update color value attribute with hex code---
 	hexcolor = "#" + ((1 << 24) + (pos1 << 16) + (pos2 << 8) + pos3).toString(16).slice(1);
+};
 
-	return  rgbColor;
-}
-
-
-// ---Update "color1" value attribute--- 
-function colorPick1() {
-	generateColor();
-	cc1.value = hexcolor;
-}
-
-// ---Update "color2" value attribute---
-function colorPick2() {
-	generateColor();
-	cc2.value = hexcolor;
-}
+// This will update "color1" value attribute--
+const colorPick1 = () => { generateColor(); color1.value = hexcolor; };
+// This will update "color2" value attribute--
+const colorPick2 = () => { generateColor(); color2.value = hexcolor; };
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
